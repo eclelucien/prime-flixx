0c9c093d5dfbb72d1c393153212d22637b9a8840
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-07-30 19:13:41 -0300

    set router

---
+++ src/Filme/index.js
@@ -0,0 +1,11 @@
+function Filme(){
+    return (
+        <div>
+            <h1>
+                Bem vin a pagina detalhes do Filme.
+            </h1>
+        </div>
+    )
+}
+
+export default Filme;
---
+++ src/Home/index.js
@@ -0,0 +1,12 @@
+
+function Home(){
+    return (
+        <div>
+            <h1>
+                Welcome...
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
