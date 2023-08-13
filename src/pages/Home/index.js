b28675c98abc35871dab108a674d5bb64f215ec6
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-08-13 20:38:39 -0300

    First requisition of films ok

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,27 @@
 import { useEffect, useState } from 'react';
+import api from '../../services/api'
 
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+
+        async function loadFilmes(){
+            const response = api.get("movie/now_playing", {
+                params:{
+                    api_key: "b5598d298b1080f63e71b57690a0bb28",
+                    language: "pt-BR",
+                    page: 1
+                }
+            })
+
+            console.log(response);
+        }
+
+        loadFilmes();
+
+    },[])
+
     return (
         <div>
             <h1>
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+// baseURL: 'https://api.themoviedb.org/3/'
+// URL: /movie/now_playing?api_key=b5598d298b1080f63e71b57690a0bb28&language=pt-BR
+
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/'
 })
