295ae2dca30bbe11080209f0f56e3bb6b661385b
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-08-27 22:40:04 -0300

    chore: show specific film in progress

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,4 +1,29 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import api from '../../services/api';
+
 function Filme(){
+    const { id } = useParams();
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`,{
+                params:{
+                    api_key: "b5598d298b1080f63e71b57690a0bb28", 
+                    language: "pt-BR",
+                }
+            }).then((response)=>{
+                console.log(response)
+            })
+            .catch(()=>{
+                
+            })
+        }
+
+        loadFilme();
+    })
+
     return (
         <div>
             <h1>
