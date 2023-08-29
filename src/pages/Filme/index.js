ea2519d5fe34d412ba7b401111637e700120a330
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-08-28 23:16:22 -0300

    chore: detail screen in progress

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -5,6 +5,8 @@ import api from '../../services/api';
 
 function Filme(){
     const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
         async function loadFilme(){
@@ -14,21 +16,40 @@ function Filme(){
                     language: "pt-BR",
                 }
             }).then((response)=>{
-                console.log(response)
+                setFilme(response.data);
+                setLoading(false);
             })
             .catch(()=>{
-                
+                console.log("Filme não encontrado")
             })
         }
 
         loadFilme();
-    })
+
+        return () => {
+            console.log("Componente foi desmontado")
+        }
+
+    }, [])
+
+    if(loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
 
     return (
-        <div>
+        <div className='filme-info'>
             <h1>
-                Welcome to the film screen.
+               {filme.title}
             </h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.votre_average} / 10</strong>
         </div>
     )
 }
