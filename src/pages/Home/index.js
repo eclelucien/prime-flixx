8e72ff69726a3f548edb365d43f6dce4b89ed823
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-09-01 22:51:11 -0300

    chore: Add scroll into home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,56 +1,83 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './home.css';
 
-function Home(){
-    const [filmes, setFilmes] = useState([]);
-    const [loading, setLoading] = useState(true);
+function Home() {
+  const [filmes, setFilmes] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
+  const loadFilmes = async (pageNum) => {
+    try {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "b5598d298b1080f63e71b57690a0bb28",
+          language: "pt-BR",
+          page: pageNum,
+        },
+      });
 
-        async function loadFilmes(){
-            const response = await api.get("movie/now_playing", {
-                params:{
-                    api_key: "b5598d298b1080f63e71b57690a0bb28", 
-                    language: "pt-BR",
-                    page: 1
-                }
+      const newFilmes = response.data.results;
 
-            })
-            setFilmes(response.data.results.slice(0, 10))
-            setLoading(false)
-        }
+      // Append the new films to the existing list
+      setFilmes((prevFilmes) => [...prevFilmes, ...newFilmes]);
 
-        loadFilmes();
-
-    },[])
-
-    if(loading){
-        return(
-            <div className='loading'>
-                <h2>
-                    Carregando filmes...
-                </h2>
-            </div>
-        )
+      if (newFilmes.length === 0) {
+        // No more films available
+        setHasMore(false);
+      }
+    } catch (error) {
+      console.error("Error loading films:", error);
     }
 
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadFilmes(1);
+  }, []);
+
+  const fetchMoreData = () => {
+    setPage(page + 1);
+    loadFilmes(page + 1);
+  };
+
+  if (loading) {
     return (
-        <div className="container">
-           <div className="lista-filmes">
-            {filmes.map( (filme) => {
-                return(
-                    <article key={filme.id}>
-                        <strong>{filme.title} </strong>
-                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
-                        <Link to={`/filme/${filme.id}`}>Access</Link>
-                    </article>
-                )
-            })}
-           </div>
+      <div className='loading'>
+        <h2>Carregando filmes...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <InfiniteScroll
+        dataLength={filmes.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="lista-filmes">
+          {filmes.map((filme) => {
+            return (
+              <article key={filme.id}>
+                <strong>{filme.title} </strong>
+                <img
+                  src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                  alt={filme.title}
+                />
+                <Link to={`/filme/${filme.id}`}>Access</Link>
+              </article>
+            );
+          })}
         </div>
-    )
+      </InfiniteScroll>
+    </div>
+  );
 }
 
 export default Home;
